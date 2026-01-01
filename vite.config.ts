52a0dd417745d2229ba401be6d83d6988df4c980
GuerrinoSantoni <74312147+GuerrinoSantoni@users.noreply.github.com>
2026-01-01 10:29:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 
 export default defineConfig(({ mode }) => {
   // Carica le variabili d'ambiente dal sistema (Vercel) o dal file .env
+  // Fix for: Property 'cwd' does not exist on type 'Process'
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
